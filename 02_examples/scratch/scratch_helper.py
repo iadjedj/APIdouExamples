17f256af458f64eaeacca0570e8f0ace8f2cae0e
iadjedj <ilann@apidou.fr>
2016-12-19 15:29:09 +0100

    Adding auto reconnect to Scratch Helper

--- 02_examples/scratch/scratch_helper.py
+++ 02_examples/scratch/scratch_helper.py
@@ -17,8 +17,30 @@ import argparse
 app = Flask("hue_helper_app")
 apidou = ''
 
+def bold(s):
+	return '\033[1m' + str(s) + '\033[0m'
+
+def reconnect():
+	try:
+		apidou.adapter.stop()
+		apidou.connect()
+		apidou.setNotifyAccel(True)
+		apidou.setNotifyTouch(True)
+	except Exception as e:
+		print "Exception during reconnect", e
+		pass
+
 @app.route('/poll')
 def poll():
+	try:
+		rssi = apidou.device.get_rssi()
+		if rssi == 0:
+			reconnect()
+			return '_problem The device is disconnected. Relaunch the helper script'
+	except pygatt.exceptions.NotConnectedError:
+		reconnect()
+		return '_problem The device is disconnected. Relaunch the helper script'
+
 	msg = "touch/pied_gauche "
 	msg += "true" if apidou.isTouched(APIdou.LEFT_FOOT) else "false"
 
@@ -72,7 +94,6 @@ def poll():
 	else:
 		msg += "shake false\n"
 
-	print msg
 	return msg
 
 @app.route('/vibrate/<int:length>')
@@ -96,37 +117,42 @@ def cross_domain_check():
 """
 
 def main():
-	""" Helper to communicate with Scratch 2.0
+	""" Helper to communicate with Scratch 2.0 (offline version)
 	This code uses Flask to create a small local HTTP server on port 1337,
 	it implements the Scratch HTTP procotol (more info on :
 	https://wiki.scratch.mit.edu/w/images/ExtensionsDoc.HTTP-9-11.pdf )
 	"""
-	logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+	logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
 
-	print "Launching scan..."
+	print bold("==========================================")
+	print bold("            APIdou > Scratch              ")
+	print bold("==========================================")
+	print "Launching BLE scan, please wait..."
 	scan_result = APIdou.scan("bled112", timeout=5)
 
-	for i in range(0, len(scan_result)):
-		print i, ") Name :", scan_result[i]['name'], "/ Address: ", scan_result[i]['address']
+	i = 1
+	for dev in scan_result:
+		print bold(i), ") Name:", bold(dev['name'] or "(No Name)")
 		i += 1
-
 	nb = raw_input("Please enter the number corresponding to your APIdou : ")
 
 	try:
-		choice = int(nb)
+		choice = int(nb) - 1
 	except ValueError:
 		print "Please enter a number"
 		return
-	if choice not in range(0, len(scan_result)):
+	if choice < 0 or choice > len(scan_result) - 1:
 		print "Not a valid choice"
 		return
+	else:
+		print "\nConnecting to", bold(scan_result[choice]['name'])
 
 	try:
 		global apidou
 
 		apidou = APIdou("bled112", scan_result[choice]['address'])
 		apidou.connect()
-		print "Connected to APIdou"
+		print "Connected to", bold(scan_result[choice]['name'])
 
 		# Make the APIdou vibrate for 100ms to check if connection is ok
 		apidou.setVibration(True)
@@ -136,11 +162,11 @@ def main():
 		apidou.setNotifyAccel(True)
 		apidou.setNotifyTouch(True)
 
-		print "APIdou helper launched"
+		print "HTTP extension launched"
 		print "Press Control + C to quit"
 		app.run('0.0.0.0', port=1337)
-	except pygatt.exceptions.NotConnectedError:
-		print "Could not connect. Check if device is on (program will exit)"
+	# except pygatt.exceptions.NotConnectedError:
+		# print "Could not connect. Check if device is on (program will exit)"
 	except KeyboardInterrupt:
 		print "\nCtrl-C pressed. Goodbye!"
 	except Exception as e:
